6a89ed23c023c607474061a909f27182a54069be
Tunckalkan <seninmailin@example.com>
2026-06-21 10:05:32 +0300

    next config tip hatasi duzeltmesi

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   allowedDevOrigins: [
     "192.168.1.135",
   ],
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+} as NextConfig;
 
 module.exports = nextConfig;
 export default nextConfig;
